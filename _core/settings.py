b3d11d01d3866b45e5b6ff324d86a6f0798ea3d4
luis-mendes-95 <luis.mendes.95@hotmail.com>
2023-05-02 12:09:54 -0300

    configured actions for common users cannot post in staff dependencies

--- _core/settings.py
+++ _core/settings.py
@@ -13,7 +13,9 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 from pathlib import Path
 import os
 import dotenv
-import ipdb
+from datetime import timedelta
+import dj_database_url
+from django.core.management.utils import get_random_secret_key
 
 dotenv.load_dotenv()
 
@@ -25,13 +27,23 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv("SECRET_KEY")
+SECRET_KEY = os.getenv("SECRET_KEY", get_random_secret_key())
+# ipdb.set_trace()
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
 ALLOWED_HOSTS = []
 
+RENDER_EXTERNAL_HOSTNAME = os.getenv("RENDER_EXTERNAL_HOSTNAME")
+RAILWAY_STATIC_URL = os.getenv("RAILWAY_STATIC_URL")
+
+
+if RENDER_EXTERNAL_HOSTNAME:
+    ALLOWED_HOSTS += [RENDER_EXTERNAL_HOSTNAME, "0.0.0.0"]
+
+if RAILWAY_STATIC_URL:
+    ALLOWED_HOSTS += [RAILWAY_STATIC_URL, "0.0.0.0"]
 
 # Application definition
 
@@ -45,15 +57,18 @@ DJANGO_APPS = [
 ]
 
 MY_APPS = [
-	"accounts",
-    "cpf_cnpj"
-]
-	
-THIRD_PARTY_APPS = [
-	"rest_framework"
+    "accounts",
+    "cpf_cnpj",
 ]
 
+THIRD_PARTY_APPS = [
+    "rest_framework",
+    "drf_spectacular",
+]
+
+
 INSTALLED_APPS = DJANGO_APPS + MY_APPS + THIRD_PARTY_APPS
+
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
@@ -63,6 +78,7 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
+    "whitenoise.middleware.WhiteNoiseMiddleware",
 ]
 
 ROOT_URLCONF = "_core.urls"
@@ -92,15 +108,48 @@ WSGI_APPLICATION = "_core.wsgi.application"
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.postgresql",
-        "USER": os.getenv("POSTGRES_DB_USERNAME"),
+        "USER": os.getenv("POSTGRES_USERNAME"),
         "PASSWORD": os.getenv("POSTGRES_PASSWORD"),
         "NAME": os.getenv("POSTGRES_DB_NAME"),
         "HOST": os.getenv("POSTGRES_DB_HOST"),
-        "PORT": os.getenv("POSTGRES_DB_PORT")
+        "PORT": os.getenv("POSTGRES_DB_PORT"),
+    },
+    "outro_nome": {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": BASE_DIR / "db.sqlite3",
     },
 }
 
-AUTH_USER_MODEL = "accounts.Account"
+DATABASE_URL = os.getenv("DATABASE_URL")
+# postgres://username:password@host:port/database
+"""
+"default": {
+    "ENGINE": "django.db.backends.postgresql",
+    "USER": username,
+    "PASSWORD": password,
+    "NAME": database,
+    "HOST": host,
+    "PORT": port,
+}
+"""
+
+
+if DATABASE_URL:
+    db_from_env = dj_database_url.config(
+        default=DATABASE_URL,
+        conn_max_age=500,
+        ssl_require=True,
+    )
+
+    DATABASES["default"].update(db_from_env)
+    DEBUG = False
+
+
+if not DEBUG:
+    STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+    STATICFILES_STORAGE = (
+        "whitenoise.storage.CompressedManifestStaticFilesStorage"
+    )
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
@@ -120,6 +169,22 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
+REST_FRAMEWORK = {
+    "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.PageNumberPagination",
+    "PAGE_SIZE": 5,
+    "DEFAULT_SCHEMA_CLASS": "drf_spectacular.openapi.AutoSchema",
+}
+
+SPECTACULAR_SETTINGS = {
+    "TITLE": "Project01 API",
+    "DESCRIPTION": "API for project01 requests",
+    "VERSION": "0.0.1",
+}
+
+SIMPLE_JWT = {
+    "ACCESS_TOKEN_LIFETIME": timedelta(days=1),
+    "REFRESH_TOKEN_LIFETIME": timedelta(days=7),
+}
 
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
@@ -142,3 +207,5 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+AUTH_USER_MODEL = "accounts.Account"
--- utils/base_views.py
+++ utils/base_views.py
@@ -1,7 +1,8 @@
-from rest_framework.views import APIView, Request, Response, status
+from rest_framework.views import Request, Response, status
+from django.shortcuts import get_object_or_404
 
 
-class ListBaseView(APIView):
+class ListBaseView:
     def list(self, request: Request) -> Response:
         queryset = self.view_queryset.all()
         serializer = self.view_serializer(queryset, many=True)
@@ -9,10 +10,47 @@ class ListBaseView(APIView):
         return Response(serializer.data, status.HTTP_200_OK)
 
 
-class CreateBaseView(APIView):
+class CreateBaseView:
     def create(self, request: Request) -> Response:
         serializer = self.view_serializer(data=request.data)
         serializer.is_valid(raise_exception=True)
         serializer.save()
 
         return Response(serializer.data, status.HTTP_201_CREATED)
+
+
+class RetrieveBaseView:
+    def retrieve(
+        self, request: Request, *args: tuple, **kwargs: dict
+    ) -> Response:
+        url_param = kwargs.get(self.url_params_name)
+        obj = get_object_or_404(self.view_queryset, pk=url_param)
+        serializer = self.view_serializer(obj)
+
+        return Response(serializer.data, status.HTTP_200_OK)
+
+
+class UpdateBaseView:
+    def update(
+        self, request: Request, *args: tuple, **kwargs: dict
+    ) -> Response:
+        url_param = kwargs.get(self.url_params_name)
+        obj = get_object_or_404(self.view_queryset, pk=url_param)
+
+        serializer = self.view_serializer(obj, request.data, partial=True)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+
+        return Response(serializer.data, status.HTTP_200_OK)
+
+
+class DestroyBaseView:
+    def destroy(
+        self, request: Request, *args: tuple, **kwargs: dict
+    ) -> Response:
+        url_param = kwargs.get(self.url_params_name)
+
+        obj = get_object_or_404(self.view_queryset, pk=url_param)
+        obj.delete()
+
+        return Response(status=status.HTTP_204_NO_CONTENT)
